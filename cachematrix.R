bc03341153d4c595897f43488835222ca3df680d
Ryan Galloway <michaelrgalloway@gmail.com>
2017-09-21 12:43:01 -0700

    push1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Caching datatype for caching inverse types of matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  c <- NULL
+  setM <- function(m){
+    x <<- m
+    c <<- NULL
+  }
+  getM <- function()x
+  setI <- function(i) c <<- i
+  getI <- function()i
+  list(setMatrix = setM, getMatrix = getM, 
+       getInverse = getI,setInverse = setI)
 }
 
 
-## Write a short comment describing this function
+## Attempte to lookup the inverse in a cache, otherwise compute inverse and store in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getInverse() 
+  if(!is.null(m))return(m)
+  data<-x$get()
+  m<-solve(data) %*% data
+  x$setInverse(m)
+  m
+  
 }
